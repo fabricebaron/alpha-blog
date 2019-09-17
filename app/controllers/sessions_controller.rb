b6fdabc1efb291cea0cd072c148ba91737944969
Harry du rails <Fabricebaron@hotmail.com>
2019-09-17 05:00:19 +0200

    create & destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,20 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		debugger
-		render 'new'
-
+		user = User.find_by(email: params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			flash[:success] = "Felicitations de cette connexion !"
+			session[:user_id] = user.id
+			redirect_to user_path(user)
+		else
+			flash.now[:danger] = "Identifiant incorrect"
+			render 'new'
+		end
 	end
 
 	def destroy
-
+		session[:user_id] = nil
+		flash[:success] = "Au revoir"
+		redirect_to root_path
 	end
 end
