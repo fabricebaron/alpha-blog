45292975d82307faf4092da86cecda24bbf9f6fc
Harry du rails <Fabricebaron@hotmail.com>
2019-09-18 04:47:02 +0200

    connection apres inscription

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:success] = "New user #{@user.username} created"
-			redirect_to articles_path
+			redirect_to user_path(@user)
 		else
 			render 'new'
 		end
@@ -50,7 +51,7 @@ class UsersController < ApplicationController
 
 	def require_same_user
 		if current_user != @user
-			flash[:danger] = "Ce n'est pas ton profil"
+			flash[:danger] = "Ce n'est pas ton profil !"
 			redirect_to root_path
 		end
 	end
