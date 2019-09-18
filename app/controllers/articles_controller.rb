c7d165a8675d53bfd914c19d2e47aac9103c2a51
Harry du rails <Fabricebaron@hotmail.com>
2019-09-18 04:32:53 +0200

    restrictions added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit, :update, :show, :destroy]
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 
 	def index
@@ -16,7 +18,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-		@article.user = User.first
+		@article.user = current_user
 		if @article.save
 			flash[:success] = "Article bel et bien crée"
 			redirect_to article_path(@article)
@@ -56,4 +58,10 @@ class ArticlesController < ApplicationController
 		params.require(:article).permit(:title, :description)
 	end
 
+	def require_same_user
+		if current_user != @article.user
+			flash[:danger] = "Ce n'est pas ton article"
+			redirect_to root_path
+		end
+	end
 end
